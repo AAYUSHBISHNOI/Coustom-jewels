47b035143ed99ad8fc619f5014965a8cd4d7f259
AAYUSH BISHNOI <aayushkaswan79@gmail.com>
2023-08-28 16:33:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['dist/index.html'],
+  theme: {
+    extend: {
+
+fontFamily:{
+  'poppins':["Poppins"],
+  'play':["Playfair Display"]
+}
+    },
+  },
+  plugins: [],
+}
+
